363ff8b5461836e920605889c8b7769db0a9c277
Lflores2022 <launchx03687@innovaccion.mx>
2023-04-20 17:40:03 -0600

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int bestScore;
+    public int currentScore;
+
+    public int currentLevel = 0;
+
+    public static GameManager singleton;
+    void Awake()
+    {
+        if(singleton ==null)
+        {
+            singleton =this;
+        }
+        else if (singleton!=this)
+        {
+            Destroy(gameObject);
+        }
+
+        bestScore = PlayerPrefs.GetInt("HighScore");
+    }
+
+   public void NextLevel()
+    {
+        currentLevel++;
+        FindObjectOfType<BallController>().ResetBall();
+        Debug.Log("Pasamos de Nivel");
+    
+    }
+
+    public void RestartLevel()
+    {
+        Debug.Log("Restart");
+        singleton.currentScore = 0;
+        FindObjectOfType<BallController>().ResetBall();
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        currentScore += scoreToAdd;
+
+        if(currentScore>bestScore)
+        {
+            bestScore =currentScore;
+            PlayerPrefs.SetInt("HighScore", currentScore);
+        }
+    }
+}
